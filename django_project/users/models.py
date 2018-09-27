c5d006af9ace0ebe32d2f0c0cd93990e1c5f2d4f
Mark Nguyen <marknltv@comcast.net>
2018-09-27 12:49:57 -0700

    fixed PIL AttributeError passing bogus height

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -15,8 +15,11 @@ class Profile(models.Model):
 	def save(self):
 		super().save()
 		img = Image.open(self.image.path)
-		if img.height > 300 or img.width > 300:
-			output_size = (300, 300)
-			img.thumbnail(output_size)
-			img.save(self.image.path)
+		try:
+			if img.height > 300 or img.width > 300:
+				output_size = (300, 300)
+				img.thumbnail(output_size)
+				img.save(self.image.path)
+		except AttributeError:
+			pass 
 
